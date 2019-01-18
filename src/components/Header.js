762b6633691fcc1af499a9a0db5d6eaa9abfeeb2
unknown <chris.a.desilva@gmail.com>
2019-01-17 20:18:30 -0600

    Add react-bootstrap, link navbar to components, create LIP component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,96 +1,32 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
-import AccountCircle from '@material-ui/icons/AccountCircle';
-import MenuItem from '@material-ui/core/MenuItem';
-import Menu from '@material-ui/core/Menu';
+import { Navbar, NavItem, Nav, NavDropdown, MenuItem, Thumbnail } from 'react-bootstrap';
 
-const styles = {
-  root: {
-    flexGrow: 1,
-  },
-  grow: {
-    flexGrow: 1,
-  },
-  menuButton: {
-    marginLeft: -12,
-    marginRight: 20,
-  },
-};
-
-class Header extends React.Component{
-  state = {
-    auth: true,
-    anchorEl: null,
-  };
-
-  handleChange = event => {
-    this.setState({ auth: event.target.checked });
-  };
-
-  handleMenu = event => {
-    this.setState({ anchorEl: event.currentTarget });
-  };
-
-  handleClose = () => {
-    this.setState({ anchorEl: null });
-  };
-  
-  render() {
-    const { classes } = this.props;
-    const { auth, anchorEl } = this.state;
-    const open = Boolean(anchorEl);
-    return (
-        <AppBar>
-          <Toolbar>
-            <IconButton className={styles.menuButton} color="inherit" aria-label="Menu">
-              <MenuIcon />
-            </IconButton>
-            <Typography variant="h6" color="inherit" className={styles.grow}>
-              Mouph Management
-            </Typography>
-            <div>
-                <IconButton
-                  aria-owns={open ? 'menu-appbar' : undefined}
-                  aria-haspopup="true"
-                  onClick={this.handleMenu}
-                  color="inherit"
-                >
-                  <AccountCircle />
-                </IconButton>
-                <Menu
-                  id="menu-appbar"
-                  anchorEl={this.state.anchorEl}
-                  anchorOrigin={{
-                    vertical: 'top',
-                    horizontal: 'right',
-                  }}
-                  transformOrigin={{
-                    vertical: 'top',
-                    horizontal: 'right',
-                  }}
-                  open={open}
-                  onClose={this.handleClose}
-                >
-                  <MenuItem onClick={this.handleClose}>Profile</MenuItem>
-                  <MenuItem onClick={this.handleClose}>My account</MenuItem>
-                </Menu>
-              </div>
-            <NavLink to="/" className="navLink" activeClassName="is-active" exact={true}>Home</NavLink>
-            <NavLink to="/lip" className="navLink" activeClassName="is-active">LIP</NavLink>
-            <NavLink to="/thebanisters" className="navLink" activeClassName="is-active">The Banisters</NavLink>
-            <NavLink to="/youthandcanvas" className="navLink" activeClassName="is-active">Youth & Canvas</NavLink>
-          </Toolbar>
-        </AppBar>
-    )
-  }
-} 
-
+const Header = () => (
+  <Navbar collapseOnSelect fixedTop>
+    <Navbar.Header>
+    <Navbar.Brand>
+      <NavLink to="/" className="navLink" activeClassName="is-active" exact={true}>Mouph</NavLink>
+    </Navbar.Brand>
+    <Navbar.Toggle />
+  </Navbar.Header>  
+  <Navbar.Collapse>
+      <Nav>
+        <NavItem eventKey={1} href="#">
+          <NavLink to="/" className="navLink" activeClassName="is-active" exact={true}>Home</NavLink>
+        </NavItem>
+        <NavItem eventKey={2} href="#">
+          <NavLink to="/lip" className="navLink" activeClassName="is-active">LIP</NavLink>
+        </NavItem>
+        <NavItem eventKey={3} href="#">
+          <NavLink to="/thebanisters" className="navLink" activeClassName="is-active">The Banisters</NavLink> 
+        </NavItem>
+        <NavItem eventKey={4} href="#">
+          <NavLink to="/youthandcanvas" className="navLink" activeClassName="is-active">Youth & Canvas</NavLink>
+        </NavItem>
+      </Nav>
+    </Navbar.Collapse>
+  </Navbar>
+)
 
 export default Header;
